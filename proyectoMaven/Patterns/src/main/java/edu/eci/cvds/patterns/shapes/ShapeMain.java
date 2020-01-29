1b424139f0216272bbd709c66b5799c21ef6c16d
sanloso <sanloso25@hotmail.com>
2020-01-28 23:04:37 -0500

    Update ShapeMain.java

--- proyectoMaven/Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeMain.java
+++ proyectoMaven/Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeMain.java
@@ -1,16 +1,13 @@
 package edu.eci.cvds.patterns.shapes;
 
 public class ShapeMain {
-    private static ShapeFactory shapeFactory;
     public static void main(String[] args) {
-      shapeFactory= null;
         if (args == null || args.length != 1) {
             System.err.println("Parameter of type RegularShapeType is required.");
             return;
         }
         try {
 
-            shapeFactory= new ShapeFactory();
             RegularShapeType type = RegularShapeType.valueOf(args[0]);
             Shape shape = ShapeFactory.create(type);
 
